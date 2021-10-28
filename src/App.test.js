ca7b1bbc85fda13923e33dc86c91adcfab590085
mrjacobsullivan <mrjacobsullivan@gmail.com>
2021-10-28 14:16:17 -0500

    added App.test.js

---
+++ src/App.test.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import App from './App'
+
+test('renders without errors', () => {
+  render(<App />)
+})
+
+test('when mounts header is rendered', () => {})
